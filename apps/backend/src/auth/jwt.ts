22bcef2c8d484d9077962a187c995f2b2ef49f0b
naji kanounji <n-kanounji@hotmail.com>
2025-07-26 20:13:50 +0200

    feat: implement JWT, in server-side

---
+++ apps/backend/src/auth/jwt.ts
@@ -0,0 +1,42 @@
+import jwt from "jsonwebtoken";
+import { config } from "dotenv";
+
+config(); // Load environment variables from .env file
+const secret = process.env.JWT_SECRET;
+if (!secret) {
+  throw new Error("JWT_SECTRET is not defined in the environment variables");
+}
+
+function sign(userId: string, email: string): string {
+  const options: jwt.SignOptions = {
+    algorithm: "HS256", //using SHA-256 hash algorithm (default)
+    expiresIn: "5d",
+  };
+  const token = jwt.sign({ userId, email }, secret!, options);
+  if (!token) {
+    console.error("Failed to sign JWT token");
+    return "";
+  }
+  return token;
+}
+
+function verify(token: string) {
+  try {
+    const decoded = jwt.verify(token, secret!, {
+      algorithms: ["HS256"],
+    });
+    if (decoded) {
+      return decoded;
+    }
+    console.error("Invalid token");
+    return null;
+  } catch (error) {
+    console.error("JWT verification failed:");
+    return null;
+  }
+}
+
+export const jwtUtils = {
+  sign,
+  verify,
+};
--- packages/trpc-contract/src/context.ts
+++
@@ -1,5 +0,0 @@
-// It defines the context type used in tRPC routers
-export type Context = {
-  db: String; // Replace with actual database connection type
-  user?: { id: string; email: string }; // Optional, if logged in
-};

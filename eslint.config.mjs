8640e970bf2a16cd4357c376dd4e47daac1df3c7
Naji Kanounji <n-kanounji@hotmail.com>
2025-08-03 16:55:45 +0200

    fix: switch eslint from strict to recommended

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,6 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   eslint.configs.recommended,
-  tseslint.configs.strict,
+  tseslint.configs.recommended,
   tseslint.configs.stylistic,
 );

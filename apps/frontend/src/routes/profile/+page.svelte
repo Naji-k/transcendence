9bea5467e285f346ce85ead83d697f2843053b46
alexkasiot <alex_kasiot@hotmail.com>
2025-08-28 13:11:53 +0200

    aby steps in understanding how to make the profile page

---
+++ apps/frontend/src/routes/profile/+page.svelte
@@ -0,0 +1,50 @@
+<main class="mt-[200px] mb-[200px] px-4 max-w-[1000px] mx-auto">
+	<!-- Section of info with alias and avatar on the left and wins/losses on the right of the page -->
+	<header class="flex justify-between items-center">
+		<section class="flex items-center">
+			<img class="w-20 h-20 rounded-full mr-4" src="https://thisishollywood.com/cdn/shop/products/2330-2325_1024x1024.jpg?v=1615010563" alt="user_avatar"/>
+			<h2 class="text-3xl">Alias</h2>
+		</section>
+		<section>
+			<h2 class="text-3xl">Stats</h2>
+			<dl class="grid grid-cols-2 gap-x-4 gap-y-2">
+				<dt>Wins</dt>
+				<dd>10</dd>
+				<dt>Losses</dt>
+				<dd>15</dd>
+				<dt>Winrate</dt>
+				<dd>66.6%</dd>
+			</dl>
+		</section>
+	</header>
+	
+	<!-- Lobbies and Tournaments buttons section - They redirect to the corresponding sections below -->
+	 <nav>
+		<button>Lobbies</button>
+		<button>Tournaments</button>
+	 </nav>
+
+	 <!-- Match history section - scrollable -->
+	 <section>
+		<h3 class="text-2xl">Match history</h3>
+	 </section>
+
+	 <!-- Friends list section - scrollable -->
+	 <section>
+		<h3 class="text-2xl">Friends</h3>
+	 </section>
+
+	 <!-- Available lobbies section - Scrollable -->
+	 <section>
+		<h3 class="text-2xl">Lobbies</h3>
+	 </section>
+
+	 <!-- Available tournaments section - Scrollable -->
+	 <section>
+		<h3 class="text-2xl">Tournaments</h3>
+	 </section>
+</main>
+
+
+
+
--- apps/frontend/src/svelte_objects/editor.svelte
+++ apps/frontend/src/svelte_objects/editor.svelte
@@ -15,4 +15,4 @@
 </script>
 
 <canvas bind:this={canvas} width="1920" height="1080"
-	style="width:100vw;height:100vh;display:block;border:2px solid #ffe97c;background:#000;" />
+	style="width:100vw;height:100vh;display:block;border:2px solid #ffe97c;background:#000;"></canvas>

010bd2317fe39094852538d1b8e55eef2c1edb8f
Alexandros Kasiotakis <akasiota@student.codam.nl>
2025-08-29 15:35:28 +0200

    started adding colors

--- apps/frontend/src/routes/profile/+page.svelte
+++ apps/frontend/src/routes/profile/+page.svelte
@@ -1,49 +1,51 @@
-<main class="my-8 sm:my-16 md:my-32 lg:my-48 px-4 max-w-[1000px] mx-auto">
-	<!-- Section of info with alias and avatar on the left and wins/losses on the right of the page -->
-	<header class="flex justify-between items-center">
-		<section class="flex items-center">
-			<img class="w-20 h-20 rounded-full mr-4" src="https://thisishollywood.com/cdn/shop/products/2330-2325_1024x1024.jpg?v=1615010563" alt="user_avatar"/>
-			<h2 class="text-3xl">Alias</h2>
-		</section>
-		<section>
-			<h2 class="text-3xl">Stats</h2>
-			<dl class="grid grid-cols-2 gap-x-4 gap-y-2">
-				<dt>Wins</dt>
-				<dd>10</dd>
-				<dt>Losses</dt>
-				<dd>15</dd>
-				<dt>Winrate</dt>
-				<dd>66.6%</dd>
-			</dl>
-		</section>
-	</header>
-	
-	<!-- Lobbies and Tournaments buttons section - They redirect to the corresponding sections below -->
-	 <nav>
+<div class= "min-h-screen bg-gradient-to-br from-[#0f2027] via-[#203a43] to-[#2c5364]">
+	<main class="py-8 sm:py-16 md:py-32 lg:py-48 px-4 min-h-screen max-w-[1000px] mx-auto font-['Press_Start_2P']">
+		<!-- Section of info with alias and avatar on the left and wins/losses on the right of the page -->
+		<header class="flex justify-between items-center text-gray-300">
+			<section class="flex items-center">
+				<img class="w-20 h-20 rounded-full mr-4" src="https://thisishollywood.com/cdn/shop/products/2330-2325_1024x1024.jpg?v=1615010563" alt="user_avatar"/>
+				<h2 class="text-3xl">Alias</h2>
+			</section>
+			<section>
+				<h2 class="text-3xl pb-2">Stats</h2>
+				<dl class="grid grid-cols-2 gap-x-4 gap-y-2">
+					<dt class="text-green-400">Wins</dt>
+					<dd>10</dd>
+					<dt class="text-red-400">Losses</dt>
+					<dd>15</dd>
+					<dt class="text-cyan-400">Winrate</dt>
+					<dd>40%</dd>
+				</dl>
+			</section>
+		</header>
+		
+		<!-- Lobbies and Tournaments buttons section - They redirect to the corresponding sections below -->
+		<nav>
 		<button>Lobbies</button>
 		<button>Tournaments</button>
-	 </nav>
+		</nav>
 
-	 <!-- Match history section - scrollable -->
-	 <section>
+		<!-- Match history section - scrollable -->
+		<section>
 		<h3 class="text-2xl">Match history</h3>
-	 </section>
+		</section>
 
-	 <!-- Friends list section - scrollable -->
-	 <section>
+		<!-- Friends list section - scrollable -->
+		<section>
 		<h3 class="text-2xl">Friends</h3>
-	 </section>
+		</section>
 
-	 <!-- Available lobbies section - Scrollable -->
-	 <section>
+		<!-- Available lobbies section - Scrollable -->
+		<section>
 		<h3 class="text-2xl">Lobbies</h3>
-	 </section>
+		</section>
 
-	 <!-- Available tournaments section - Scrollable -->
-	 <section>
+		<!-- Available tournaments section - Scrollable -->
+		<section>
 		<h3 class="text-2xl">Tournaments</h3>
-	 </section>
-</main>
+		</section>
+	</main>
+</div>
 
 
 

e4fef7b15514ad9e6116a5ea13b762348b8e284c
Naji Kanounji <n-kanounji@hotmail.com>
2025-10-28 03:38:02 +0100

    fix: drizzle.config and db path, to be in /backend for production

--- apps/backend/src/db/drizzle.config.ts
+++ apps/backend/src/db/drizzle.config.ts
@@ -1,18 +1,14 @@
 import { config } from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
 import path from 'path';
-import { fileURLToPath } from 'url';
 
 config({ path: path.resolve(process.cwd(), '.env') }); // Changed it to process.cwd() because I think it makes it more clear it's at the root of the backend.
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 export default defineConfig({
-  out: 'src/db/drizzle',
-  schema: path.resolve(__dirname, '../../../../packages/db/dbSchema.ts'),
+  out: './src/db/drizzle',
+  schema: path.resolve(process.cwd(), '../../packages/db/src/dbSchema.ts'),
   dialect: 'sqlite',
   dbCredentials: {
-    url: path.resolve(__dirname, process.env.DB_FILE_NAME || 'database.sqlite'), // This will create the database file in the apps/backend/src/db folder, easily modifiable
+    url: path.resolve(process.cwd(), process.env.DB_FILE_NAME || 'database.sqlite'), // This will create the database file in the apps/backend/src/db folder, easily modifiable
   },
 });
--- apps/backend/src/db/src/dbClientInit.ts
+++ apps/backend/src/db/src/dbClientInit.ts
@@ -4,7 +4,7 @@ import { createClient } from '@libsql/client';
 import { drizzle } from 'drizzle-orm/libsql';
 
 config({ path: path.resolve(process.cwd(), '.env') }); // This works only if the .env used is in the root of the backend.
-const dbPath = process.env.DB_FILE_PATH || './src/db/database.sqlite';
+const dbPath = process.env.DB_FILE_PATH || './database.sqlite';
 
 const absolutePath = path.resolve(process.cwd(), dbPath);
 
